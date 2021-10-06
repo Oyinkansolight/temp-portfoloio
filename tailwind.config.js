cd3a6732554851703afb2e2f22d1d8a1601e6714
michael-andreuzza <michaelandreuzzavega@gmail.com>
2021-10-06 18:17:05 +0300

    overall cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
 const colors = require('tailwindcss/colors');
+//this plugin ( bottom ) will apply a class on firefox,
+// in this case, it will apply opacity to a div so is not so opaque because of blur no being supported on Firefox
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   //mode: 'jit',
   //purge: ['./src/**/*.html'],
-  darkMode: 'media', // or 'media' or 'class'
+  //darkMode: 'media', // or 'media' or 'class'
   theme: {
-    
+
+
     extend: {
+
 
       colors: {
         blueGray: colors.blueGray,
         blue: colors.blue,
+        royalBlue:"#4158D0",
+        midnight:"#1d1b3c",
+        dull:"#C850C0",
+        tainoi:"#FFCC70",
       },
       fontFamily: {
         sans: [
@@ -33,6 +42,21 @@ module.exports = {
     extend: {},
   },
   plugins: [
-      require('tailwindcss-textshadow')
+    require('tailwindcss-textshadow'),
+    plugin(function ({ addVariant, e, postcss }) {
+      addVariant("firefox", ({ container, separator }) => {
+        const isFirefoxRule = postcss.atRule({
+          name: "-moz-document",
+          params: "url-prefix()",
+        });
+        isFirefoxRule.append(container.nodes);
+        container.append(isFirefoxRule);
+        isFirefoxRule.walkRules((rule) => {
+          rule.selector = `.${e(
+            `firefox${separator}${rule.selector.slice(1)}`
+          )}`;
+        });
+      });
+    }),
   ],
 }
